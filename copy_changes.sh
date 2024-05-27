06d8bb335486894420f603c606ab73517e53c921
Max Pape <maxpape@mail.uni-paderborn.de>
2024-05-27 07:05:29 +0000

    update world file for gazebo

--- copy_changes.sh
+++ copy_changes.sh
@@ -2,3 +2,4 @@
 
 rsync -a /drone_mt_ros2/px4_changes/4006_gz_px4vision /drone_mt_ros2/PX4-Autopilot/ROMFS/px4fmu_common/init.d-posix/airframes/4006_gz_px4vision
 rsync -a /drone_mt_ros2/px4_changes/dds_topics.yaml /drone_mt_ros2/PX4-Autopilot/src/modules/uxrce_dds_client/dds_topics.yaml
+rsync -a /drone_mt_ros2/px4_changes/windy.sdf /drone_mt_ros2/PX4-Autopilot/Tools/simulation/gz/worlds/windy.sdf
